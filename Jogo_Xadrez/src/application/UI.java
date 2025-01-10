c8816366b840aa56c1cc2d19f831824466d090ae
Breno Nunes <166417202+brenonun3s@users.noreply.github.com>
2025-01-10 15:57:11 -0300

    Add files via upload
    Projeto final
    
    Implementado toda a lógica de um jogo de Xadrez

--- Jogo_Xadrez/src/application/UI.java
+++ Jogo_Xadrez/src/application/UI.java
@@ -14,6 +14,7 @@ import chess.Color;
 public class UI {
 
 	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -23,6 +24,7 @@ public class UI {
 	public static final String ANSI_PURPLE = "\u001B[35m";
 	public static final String ANSI_CYAN = "\u001B[36m";
 	public static final String ANSI_WHITE = "\u001B[37m";
+
 	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
 	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
 	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
@@ -44,31 +46,26 @@ public class UI {
 			char column = s.charAt(0);
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
-		}
-
-		catch (RuntimeException e) {
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
 		}
 	}
-	
+
 	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
 		printBoard(chessMatch.getPieces());
 		System.out.println();
-		printCapturePieces(captured);
+		printCapturedPieces(captured);
 		System.out.println();
-		System.out.println("Turn :" + chessMatch.getTurn());
+		System.out.println("Turn : " + chessMatch.getTurn());
 		if (!chessMatch.getCheckMate()) {
 			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 			if (chessMatch.getCheck()) {
-				System.out.println("XEQUE!");
+				System.out.println("CHECK!");
 			}
-			
-		}
-		else {
+		} else {
 			System.out.println("CHECKMATE!");
-			System.out.println("WINNER: " + chessMatch.getCurrentPlayer());
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
-
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -82,16 +79,21 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 	}
 
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+
 	private static void printPiece(ChessPiece piece, boolean background) {
 		if (background) {
 			System.out.print(ANSI_BLUE_BACKGROUND);
 		}
-		if (piece == null) {
-			System.out.print("-");
-		} else {
-			System.out.print(piece);
-		}
-		System.out.print(" ");
 		if (piece == null) {
 			System.out.print("-" + ANSI_RESET);
 		} else {
@@ -103,30 +105,20 @@ public class UI {
 		}
 		System.out.print(" ");
 	}
-	
-	private static void printCapturePieces(List<ChessPiece> captured) {
-		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
-		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
-		System.out.println("Captured pieces: ");
-		System.out.println("White: ");
-		System.out.println(ANSI_WHITE);
+
+	private static void printCapturedPieces(List<ChessPiece> captured) {
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE)
+				.collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK)
+				.collect(Collectors.toList());
+		System.out.println("Captured pieces:");
+		System.out.print("White: ");
+		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
-		System.out.println(ANSI_RESET);
-		System.out.println("Black: ");
-		System.out.println(ANSI_YELLOW);
+		System.out.print(ANSI_RESET);
+		System.out.print("Black: ");
+		System.out.print(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));
-		System.out.println(ANSI_RESET);
-	}
-		
-		
-	public static void printBoard(ChessPiece[][] pieces, boolean [][] possibleMoves) {
-		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + " ");
-			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j], possibleMoves[i][j]);
-			}
-			System.out.println();
-		}
-		System.out.println("  a b c d e f g h");
+		System.out.print(ANSI_RESET);
 	}
 }
